ab8656ad7514ecaa1b5cbe68a09e9333c09bf0f1
tejask2567 <tejask2567@gmail.com>
2022-10-28 14:40:03 +0530

    Webpage kinda completed

--- webpage/new.py
+++
@@ -1,8 +0,0 @@
-from DeepImageSearch import Index, LoadData, SearchImage
-
-image_list = LoadData().from_folder(['Data'])
-# Index(image_list).Start()
-results = SearchImage().get_similar_images(
-    image_path="Data/Pisces_third_60.jpg", number_of_images=5)
-res = list(results.values())[0]
-print(res)
--- webpage/view.py
+++ webpage/view.py
@@ -1,14 +1,18 @@
 from fileinput import filename
-#from DeepImageSearch import Index, LoadData, SearchImage
+from importlib.resources import path
+from DeepImageSearch import Index, LoadData, SearchImage
 import numpy as np
 from flask import request, redirect, render_template, Flask
 import os
-import pickle
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from PIL import Image
 import io
 import base64
+from werkzeug.utils import secure_filename
+import logging
+from pathlib import Path
+import cv2 as cv
 
 app = Flask(__name__)
 
@@ -28,6 +32,9 @@ def Star():
     return render_template('Star.html')
 
 
+var_List = []
+
+
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
     if request.method == 'POST':
@@ -36,30 +43,49 @@ def predict():
         if image.filename == '':
             print("file name is invalid")
             return redirect(request.url)
-        global filename
         filename = secure_filename(image.filename)
         basedir = os.path.abspath(os.path.dirname(__file__))
+        print(basedir)
         image.save(os.path.join(
             basedir, app.config["IMAGE_UPLOADS"], filename))
+        path = os.path.join(
+            basedir, app.config["IMAGE_UPLOADS"], filename)
+        print(path)
+        global img
         img = Image.open(app.config["IMAGE_UPLOADS"]+"/" + filename)
         data = io.BytesIO()
         img.save(data, "JPEG")
+        # data.seek(0)
+        #plot_url = base64.b64encode(img.getvalue()).decode()
+        #im_source = "data:Images/jpeg;base64,{}".format(plot_url)
+        #image_list = LoadData().from_folder(['Data'])
+        # Index(image_list).Start()
+        results = SearchImage().get_similar_images(
+            image_path=path, number_of_images=1)
+        global res
+        res = list(results.values())[0]
+        var_List.append(res)
+        #encode_img_data = base64.b64encode(data.getvalue())
 
-        encode_img_data = base64.b64encode(data.getvalue())
-
-        return render_template("index.html", filename=encode_img_data.decode("UTF-8"))
+        return render_template("index.html", filename=filename)
 
     return render_template('index.html')
 
 
-""" @app.route('/search', methods=['POST', 'GET'])
+""" image_list = LoadData().from_folder(['Data'])
+Index(image_list).Start()
+results = SearchImage().get_similar_images(
+    image_path=(os.path.abspath(img)), number_of_images=1)
+res = list(results.values())[0] """
+
+
+@app.route('/search', methods=['POST'])
 def search():
     if request.method == 'POST':
-        image_list = LoadData().from_folder(['Data'])
-        Index(image_list).Start()
-        results = SearchImage().get_similar_images(
-            image_path=["IMAGE_UPLOADS"]+"/" + filename, number_of_images=5)
-        print(results) """
+        res2 = var_List.pop()
+        processed_text = res2.split('\\')[1].split('_')[0]
+        return render_template('index.html', processed_text=processed_text)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
